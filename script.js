7b4bbd2a9b65bbf9ba514ab95cb94bf97d62b8ea
szoker <szoker527@gmail.com>
2023-02-10 11:18:12 +0100

    Add module it displays gameArray

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //
 const gameBoard = {
-    gameArray: []
+  gameArray: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+};
 
-}
+const newPlayer = function (name, weapon) {
+  return {
+    name,
+    weapon,
+  };
+};
 
-const newPlayer = function(name, weapon) {
+const playerO = newPlayer("playerOne", "O");
+const playerX = newPlayer("player   Two", "X");
 
-}
+const displayAction= (() => {
+    let marker 
+    const page = document.querySelectorAll(".game");
+    const buttons = document.querySelectorAll(".btn");
+    const showArray = (array) => {
+      for (let index = 0; index < page.length; index++) {
+        page[index].addEventListener("click", function () {
+          console.log(array[index])
+        });
+        page[index].innerHTML = array[index];
+      }
+    };
+    const newMakrer = () => {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function() {
+                marker = buttons[i].id
+              console.log(marker);
+            })
+        }
+    }
+    return {
+      showArray,
+      newMakrer
+
+    };
+  })();
+
+displayAction.showArray(gameBoard.gameArray);
+displayAction.newMakrer()
