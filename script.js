e0f4d58e71b1f9ac434ec21a44cabe2770ff6c54
Szoker527 <szoker527@gmail.com>
2023-02-21 16:22:31 +0100

    Add h1 to show player turn name

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function gameBoard () {
 }
 
 function Cell () {
-  let value = 0;
+  let value
 
   const addToken = (player) => {
     value = player;
@@ -163,22 +163,27 @@ function gameControl () {
 function ScreenController() {
   const game = gameControl();
   const boardDiv = document.querySelector('.game-board');
+  const playerTurnDiv = document.querySelector('.text-display');
   
   const updateScreen = () => {
     // clear the board
     boardDiv.textContent = "";
     // get the newest version of the board and player turn
     const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
 
     // Render board squares
     board.forEach((row, indexRow) => {
-      row.forEach((cell, index) => {
+      row.forEach((cell, indexColumn) => {
         // Anything clickable should be a button!!
         const cellButton = document.createElement("button");
         cellButton.classList.add("game");
         // Create a data attribute to identify the column
         // This makes it easier to pass into our `playRound` function 
-        cellButton.dataset.column = index
+        cellButton.dataset.column = indexColumn
         cellButton.dataset.row = indexRow
         cellButton.textContent = cell.getValue();
         boardDiv.appendChild(cellButton);
